9d1f39b5bde2be58e1b44eb52d9979060fe961eb
dylansuaris <dylansuaris@gmail.com>
2021-03-31 19:01:00 +0400

    added Quit game button

--- Assets/Scripts/UI/GameOverMenu.cs
+++ Assets/Scripts/UI/GameOverMenu.cs
@@ -9,4 +9,9 @@ public class GameOverMenu : MonoBehaviour
     {
         SceneManager.LoadScene("Intro");
     }
+    public void Exit()
+    {
+        Application.Quit();
+        Debug.Log("exit");
+    }
 }
